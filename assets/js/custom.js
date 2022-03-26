1c0cdef229dab6f56035d1409eb000b80d33d4f1
wangxin1248 <wangxin1248@gmail.com>
2022-03-26 22:09:23 +0800

    增加对应目录

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -38,3 +38,50 @@ function searchHandler() {
     // add site:example.com in the placeholder
     window.location.href = "https://www.google.com/search?q=site:wangxin1248.github.io " + text;
 }
+
+
+   
+/*侧边栏显示、切换等*/
+function showCatelog(){
+	$('#markdown-toc').hide();
+	if (typeof $('#markdown-toc').html() === 'undefined') {
+		$('.a_catelog').removeClass('active');
+		$('.a_bloginfo').addClass('active');
+		$('div.sidebar_catelog').hide();
+		$('div.sidebar_index').show();
+		$('#sidebar_close').hide();
+	}else if($(window).width() >= 1200){
+		$('.sidebar_catelog').html('<ul class="list_catelog">' + $('#markdown-toc').html() + '</ul>');
+		$('#right_sidebar').animate({right:"+=300px"},300);
+		$('#content').animate({left:'-=150px'},300);
+		$('.a_bloginfo').removeClass('active');
+		$('.a_catelog').addClass('active');
+		$('div.sidebar_index').hide();
+		$('div.sidebar_catelog').show();
+	}
+}
+
+// 侧边栏目录
+function locateCatelogList(){
+    /*获取文章目录集合,可通过：header过滤器*/
+    var alis = $('.article :header');
+    /*获取侧边栏目录列表集合**/
+    var sidebar_alis = $('.sidebar_catelog').find('a');
+    /*获取滚动条到顶部的距离*/
+    var scroll_height = $(window).scrollTop();
+    for(var i =0;i<alis.length;i++){
+        /*获取锚点集合中的元素分别到顶点的距离*/
+        var a_height = $(alis[i]).offset().top;
+        if (a_height<scroll_height){
+            /*高亮显示*/
+            sidebar_alis.removeClass('list_click');
+            $(sidebar_alis[i]).addClass('list_click');
+        }
+    }
+}
+
+$(function() {
+    showCatelog();
+    /*绑定滚动事件 */ 
+    $(window).bind('scroll',locateCatelogList); 
+});
